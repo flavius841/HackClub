6d1fb1ac78af46968288dcb75b1c20969aa3842a
flavius841 <flaviusepintilie@gmail.com>
2025-10-13 08:00:38 +0300

    u

--- Assets/resources/scriptsStartMenu/SettingsScript.cs
+++ Assets/resources/scriptsStartMenu/SettingsScript.cs
@@ -2,9 +2,33 @@ using UnityEngine;
 
 public class SettingsScript : MonoBehaviour
 {
-    [SerializeField] GameObject MenuPrefab;
+    public GameObject MenuPrefab;
+    public GameObject Menu;
+    public Transform Canvass;
+    [SerializeField] float Xpos, Ypos;
+    [SerializeField] float Timer;
+    [SerializeField] float Speed;
+    void Start()
+    {
+    }
+
     public void SettingsMenu()
     {
-        GameObject Menu = Instantiate(MenuPrefab);
+        Menu = Instantiate(MenuPrefab, Canvass);
+        Menu.transform.position = new Vector3(Xpos, Ypos, 0);
+        Menu.transform.localScale += Vector3.one * Time.deltaTime;
+        Timer = 0;
+
     }
+    
+    void Update()
+    {
+        Timer = Timer + Time.deltaTime * Speed;
+        if (Timer <= 1.7f)
+        {
+            Menu.transform.localScale += Vector3.one * Time.deltaTime;
+            Menu.transform.Rotate(0, 0, 200 * Time.deltaTime);
+        }
+    }
+
 }
