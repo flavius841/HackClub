7d5205737c1965d2681fe24f49d4f400ce97f69a
flavius841 <flaviusepintilie@gmail.com>
2025-10-28 21:21:28 +0200

    Fixing Bugon choosing Cars

--- Assets/resources/scriptsRace/CarManagerScript.cs
+++ Assets/resources/scriptsRace/CarManagerScript.cs
@@ -4,25 +4,32 @@ public class CarManagerScript : MonoBehaviour
 {
     void Start()
     {
-        if (ChooseCar.Instance.SelectedCar == 1)
+        int selectedCar = 3; 
+        
+        if (ChooseCar.Instance != null)
+        {
+            selectedCar = ChooseCar.Instance.SelectedCar;
+        }
+
+        if (selectedCar == 1)
         {
             // Activate Formula Car
             transform.GetChild(0).gameObject.SetActive(true);
         }
 
-        else if (ChooseCar.Instance.SelectedCar == 2)
+        else if (selectedCar == 2)
         {
             // Activate Monster Truck
             transform.GetChild(1).gameObject.SetActive(true);
         }
 
-        else if (ChooseCar.Instance.SelectedCar == 4)
+        else if (selectedCar == 4)
         {
             // Activate Rally Car
             transform.GetChild(3).gameObject.SetActive(true);
         }
 
-        else
+        else if (selectedCar == 3)
         {
             // Activate Normal Car
             transform.GetChild(2).gameObject.SetActive(true);
@@ -32,14 +39,9 @@ public class CarManagerScript : MonoBehaviour
 
     }
     
-    // void Update()
-    // {
-    //     Debug.Log("ChooseCar instance: " + ChooseCar.Instance);
-    //     Debug.Log("SelectedCar: " + ChooseCar.Instance.SelectedCar);
+    
+    
 
-    // }
-    
-    
     
 
 
