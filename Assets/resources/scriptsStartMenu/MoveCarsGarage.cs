dad54d828ef47047f7f3c90bc9f5de9ff2e044b5
flavius841 <flaviusepintilie@gmail.com>
2025-10-28 16:04:58 +0200

    u

--- Assets/resources/scriptsStartMenu/MoveCarsGarage.cs
+++ Assets/resources/scriptsStartMenu/MoveCarsGarage.cs
@@ -9,20 +9,52 @@ public class MoveCarsGarage : MonoBehaviour
     [SerializeField] float PosX;
     [SerializeField] float MaxPosX;
     [SerializeField] float MinPosX;
+    [SerializeField] float Timer;
 
 
     public void MoveTowards()
-    {
-        Towards = true;
-        Back = false;
-        MaxPosX = transform.position.x + 15f;
+    {   
+        PosX = transform.position.x;
+        if (PosX == 30 && Timer > 0.5f)
+        {
+            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+            MaxPosX = -15;
+            Timer = 0;
+        }
+
+        else if ((PosX == -30 || PosX == -15 || PosX == 0 || PosX == 15) && Timer > 0.5f)
+        {
+            Towards = true;
+            Back = false;
+            MaxPosX = transform.position.x + 15f;
+            Timer = 0;
+
+        }
     }
 
     public void MoveBack()
     {
-        Back = true;
-        Towards = false;
-        MinPosX = transform.position.x - 15f;
+        // Back = true;
+        // Towards = false;
+        // MinPosX = transform.position.x - 15f;
+        // PosX = transform.position.x;
+
+        PosX = transform.position.x;
+        if (PosX == -45 && Timer > 0.5f)
+        {
+            transform.position = new Vector3(0, transform.position.y, transform.position.z);
+            MinPosX = 0;
+            Timer = 0;
+        }
+
+        else if ((PosX == -30 || PosX == -15 || PosX == 0 || PosX == 15) && Timer > 0.5f)
+        {
+            Back = true;
+            Towards = false;
+            MinPosX = transform.position.x - 15f;
+            Timer = 0;
+
+        }
     }
     
     void Start()
@@ -32,9 +64,10 @@ public class MoveCarsGarage : MonoBehaviour
 
     void Update()
     {
+        Timer += Time.deltaTime;
+
         if (Towards)
         {
-            
             if (PosX >= MaxPosX + 3)
             {
                 Step2 = true;
@@ -60,12 +93,27 @@ public class MoveCarsGarage : MonoBehaviour
 
         if (Back)
         {
-            PosX = Mathf.MoveTowards(transform.position.x, MinPosX, MoveSpeed * Time.deltaTime);
-            transform.position = new Vector3(PosX, transform.position.y, transform.position.z);
-            if (PosX <= MinPosX)
+            if (PosX <= MinPosX - 3)
             {
-                Towards = false;
+                Step2 = true;
             }
+
+            else if (!Step2)
+            {
+                PosX = Mathf.MoveTowards(transform.position.x, MinPosX - 3, MoveSpeed * Time.deltaTime);
+            }
+
+            if (Step2)
+            {
+                PosX = Mathf.MoveTowards(transform.position.x, MinPosX, MoveSpeed / 2 * Time.deltaTime);
+                if (PosX >= MinPosX)
+                {
+                    Back = false;
+                    Step2 = false;
+                }
+            }
+            
+            transform.position = new Vector3(PosX, transform.position.y, transform.position.z);
         }
         
     }
