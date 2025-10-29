2011e246479f97d5dc12b26d3bb1769531d53b19
flavius841 <flaviusepintilie@gmail.com>
2025-10-29 11:14:57 +0200

    Fixing Camera Bug and configuring LoadRace2 scene

--- Assets/resources/scriptsRace/SetCamera.cs
+++ Assets/resources/scriptsRace/SetCamera.cs
@@ -8,32 +8,39 @@ public class SetCamera : MonoBehaviour
     [SerializeField] GameObject MonsterTruck;
     [SerializeField] GameObject NormalCar;
     [SerializeField] GameObject RallyCar;
+    [SerializeField] bool Verified;
     
     void Update()
     {
-        if (FormulaCar.activeInHierarchy)
+        if (!Verified)
         {
-            vcam.Follow = FormulaCar.transform;
-            vcam.LookAt = FormulaCar.transform;
-        }
+            if (FormulaCar.activeInHierarchy)
+            {
+                vcam.Follow = FormulaCar.transform;
+                vcam.LookAt = FormulaCar.transform;
+            }
 
-        else if (MonsterTruck.activeInHierarchy)
-        {
-            vcam.Follow = MonsterTruck.transform;
-            vcam.LookAt = MonsterTruck.transform;
-        }
+            else if (MonsterTruck.activeInHierarchy)
+            {
+                vcam.Follow = MonsterTruck.transform;
+                vcam.LookAt = MonsterTruck.transform;
+            }
 
-        else if (NormalCar.activeInHierarchy)
-        {
-            vcam.Follow = NormalCar.transform;
-            vcam.LookAt = NormalCar.transform;
-        }
+            else if (NormalCar.activeInHierarchy)
+            {
+                vcam.Follow = NormalCar.transform;
+                vcam.LookAt = NormalCar.transform;
+            }
 
-        else
-        {
-            vcam.Follow = RallyCar.transform;
-            vcam.LookAt = RallyCar.transform;
+            else
+            {
+                vcam.Follow = RallyCar.transform;
+                vcam.LookAt = RallyCar.transform;
+            }
+            
+            Verified = true;
         }
+        
     }
 
 }
